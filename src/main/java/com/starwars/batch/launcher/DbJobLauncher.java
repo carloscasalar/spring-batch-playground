8434cdc5f6546029576a356e488e57b7f755df05
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-07-21 19:13:49 +0200

    Rename job launcher

--- src/main/java/com/starwars/batch/launcher/DbJobLauncher.java
+++ src/main/java/com/starwars/batch/launcher/DbJobLauncher.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
-public class CsvJobLauncher {
+public class DbJobLauncher {
     private JobLauncher jobLauncher;
     private Job dbJob;
 
